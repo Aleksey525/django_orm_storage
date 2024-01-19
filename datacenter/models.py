0cfdc54ef88d31f82cd2624c55be95bd9e297f4c
Aleksey525 <alex_tolchin@mail.ru>
2024-01-19 15:58:25 +0300

    upgrade models

--- datacenter/models.py
+++ datacenter/models.py
@@ -31,23 +31,7 @@ class Visit(models.Model):
         )
 
 
-# def get_duration(visit):
-#     time_now = localtime()
-#     if visit.leaved_at == None:
-#         return time_now - visit.entered_at
-#     return visit.leaved_at - visit.entered_at
-#
-#
-# def format_duration(duration):
-#     total_seconds = duration.seconds
-#     hours, remainder = divmod(total_seconds, 60 * 60)
-#     minutes, seconds = divmod(remainder, 60)
-#     return f'{hours:02}:{minutes:02}:{seconds:02}'
-#
-#
-#
-# def is_visits_long(visit, minutes=60):
-#     return get_duration(visit).total_seconds() > minutes * 60
+
 
 
 
